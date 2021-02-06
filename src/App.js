8b2e955907e115ce8e6fbca1d10a62c6a6f0659e
--global <supitcha.tree@hotmail.com>
2021-02-06 11:05:57 +0700

    Make able to send message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
+import { useState } from 'react';
 import './App.css';
+// import React, { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const [lines, setLines] = useState([]);
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () => {
+    setLines(lines => [...lines, text]);
+    setText("");
+  }
+
+  const sendOnEnter = (e) => {
+    if (e.key === 'Enter') {
+      onSend();
+    }
+  }
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
-        </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
+        {
+          lines.map(x => {
+            return <div className="App-chatroom-text">
+              {x}
+            </div>
+          }
+          )
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" value={text} placeholder="start typing to chat :)" onChange={onTextChange} onKeyDown={sendOnEnter} />
+        <div className="App-textbox-send" onClick={onSend}>Send!</div>
       </div>
     </div>
   );
