b977313211d1d87e037fd1b387087e87c12f41cf
--global <supitcha.tree@hotmail.com>
2021-03-20 10:36:11 +0700

    added useEffect make lines array of objects

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,19 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Chatbox.css';
 import App from './App';
 // import React, { useEffect, useState } from "react";
 
 function Chatbox() {
+    //Use State
     const [text, setText] = useState("");
-
     const [lines, setLines] = useState([]);
 
+    //Use Effect
+    useEffect(() => {
+        console.log("total text chat: " + lines.length)
+    }, [lines]);
+
+    //Functions
     const onTextChange = (event) => {
         setText(event.target.value);
     };
 
     const onSend = () => {
-        setLines(lines => [...lines, text]);
+        setLines(lines => [...lines, {
+            sender: "Me",
+            message: text,
+            timestamp: (new Date()),
+            classname: "default"
+        }]);
         setText("");
     }
 
@@ -22,14 +33,24 @@ function Chatbox() {
             onSend();
         }
     }
+
+    //Render
     return (
         <App>
             <div className="App-chatroom-container">
                 <div className="App-chatroom">
                     {
                         lines.map(x => {
-                            return <div className="App-chatroom-text">
-                                {x}
+                            return <div className={"App-chatroom-text App-chatroom-text-" + x.classname}>
+                                <div>
+                                    {x.sender + ":"}
+                                </div>
+                                <div>
+                                    {x.message}
+                                </div>
+                                <div>
+                                    {x.timestamp.toLocaleDateString()}
+                                </div>
                             </div>
                         }
                         )
