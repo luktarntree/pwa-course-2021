885fb7d050a96a44f1a9ad544dbee30447284d89
--global <supitcha.tree@hotmail.com>
2021-03-27 09:25:32 +0700

    pasted SDK from firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,1 +1,13 @@
 import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAOw74bMRbvhSbDwAv75F7lJKyTUW3n6Ac",
+    authDomain: "lt-pwa-class-chat-app.firebaseapp.com",
+    projectId: "lt-pwa-class-chat-app",
+    storageBucket: "lt-pwa-class-chat-app.appspot.com",
+    messagingSenderId: "891042032088",
+    appId: "1:891042032088:web:3f6862823aced6bfe46aad"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
