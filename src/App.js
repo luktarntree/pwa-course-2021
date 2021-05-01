4f42e3ec71913f6710a62477f515f63d2cf1ff14
--global <supitcha.tree@hotmail.com>
2021-05-01 10:16:43 +0700

    removed unused usestate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import './App.css';
-// import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
 function App({ children }) {
