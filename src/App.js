616b1f7486904a12947740e16b5b71bc36c0a796
--global <supitcha.tree@hotmail.com>
2021-05-02 00:54:45 +0700

    added logout function and button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { Link } from 'react-router-dom';
 
 function App({ children }) {
+  const logOut = () => {
+    localStorage.setItem('username', '')
+    localStorage.setItem('loggedin', '')
+  }
   return (
     <div className="App">
       <div className="App-header">
@@ -13,6 +17,9 @@ function App({ children }) {
         </Link>
         <Link to="/chatbox">
           <button>Chatbox</button>
+        </Link>
+        <Link to="/">
+          <button onClick={logOut}>Log Out</button>
         </Link>
       </div>
       <div className="App-content">
