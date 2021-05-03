aab76df890478b368e3f79990fd626ed40aaf3b6
--global <supitcha.tree@hotmail.com>
2021-05-03 17:17:50 +0700

    added condition of logged out

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 import './App.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Login } from './Login';
 
 function App({ children }) {
+  const [username, setUsername] = useState(localStorage.getItem('username'));
   const logOut = () => {
     localStorage.setItem('username', '')
     localStorage.setItem('loggedin', '')
   }
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Link to="/friend-list">
-          <button>Friends</button>
-        </Link>
-        <Link to="/profile">
-          <button>Profile</button>
-        </Link>
-        <Link to="/chatbox">
-          <button>Chatbox</button>
-        </Link>
+  if (username === '') {
+    return (
+      <div>
+        you are not login.
         <Link to="/">
-          <button onClick={logOut}>Log Out</button>
+          <button>Login</button>
         </Link>
       </div>
-      <div className="App-content">
-        {children}
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div>{username}</div>
+          <Link to="/friend-list">
+            <button>Friends</button>
+          </Link>
+          <Link to="/profile">
+            <button>Profile</button>
+          </Link>
+          <Link to="/chatbox">
+            <button>Chatbox</button>
+          </Link>
+          <Link to="/">
+            <button onClick={logOut}>Log Out</button>
+          </Link>
+        </div>
+        <div className="App-content">
+          {children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
