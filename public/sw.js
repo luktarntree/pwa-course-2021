c33feb5b6d812c7c0d1408266ea104a1f5ff7818
--global <supitcha.tree@hotmail.com>
2021-05-08 10:17:58 +0700

    14 added cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,9 @@
 //test ว่า install สำเร็จมั้ย
 self.addEventListener("install", (event) => {
-    console.log("SW is installing :D")
+    console.log("SW is installing v1.2")
+    //add things to cache
+    event.waitUntil(
+        caches.open('chatapp-v1.2')
+            .then((cache) => { return cache.addAll(['/', '/index.html']) }
+            ))
 })
