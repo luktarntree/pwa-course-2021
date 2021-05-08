48eb0372b6403eda8d2e6f986ab3a8d6067cd452
--global <supitcha.tree@hotmail.com>
2021-05-08 11:03:46 +0700

    14 added command for using cache

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,21 @@
-//test ว่า install สำเร็จมั้ย
 self.addEventListener("install", (event) => {
-    console.log("SW is installing v1.2")
+    //test ว่า install สำเร็จมั้ย
+    console.log("SW is installing v1.2");
     //add things to cache
     event.waitUntil(
         caches.open('chatapp-v1.2')
             .then((cache) => { return cache.addAll(['/', '/index.html']) }
             ))
+})
+
+self.addEventListener("fetch", (event) => {
+    caches.match(event.request.url) //see if cache matches with the request
+        .then((res) => {
+            if (res) {
+                console.log("Intercepted with cache", res);
+                return res //make cache work
+            }
+
+            return fetch(event.request); //if cache not match, use network
+        })
 })
